03e1b0f1a6ed238134769f4f260524db20aeefd0
rifamazharulhaq <rifamazharulhaq.dhp@gmail.com>
2025-12-29 20:59:03 +0700

    removed unnecessary csv and files and optimized the code in regist_patient and main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # ==================== MAIN PROGRAM ====================
-from pages import menu_admin, menu_dokter, menu_pasien, menu_resepsionis
+from pages import menu_admin, menu_dokter, menu_patient, menu_reseptionst
 from pages.auth import login
 from utils.init_db import init_database
 from pages.regist_patient import registrasi_pasien
@@ -28,11 +28,11 @@ def main():
             user, role = login()
             if user and role:
                 if role == "Pasien":
-                    menu_pasien(user)
+                    menu_patient(user)
                 elif role == "Dokter":
                     menu_dokter(user)
                 elif role == "Resepsionis":
-                    menu_resepsionis(user)
+                    menu_reseptionst(user)
                 elif role == "Admin":
                     menu_admin(user)
         elif pilihan == "2":
